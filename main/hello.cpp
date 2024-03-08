a17acd7c3caa27b4ae8319b88b3f56e5bbf756e3
RahulKalekar <kalekarrahul12@gmail.com>
2024-03-08 19:32:38 +0530

    Commiting .cpp changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<" I have added this lin efor webhooks";
+  cout<<" I have added this for webhooks";
   return 0;
 }
